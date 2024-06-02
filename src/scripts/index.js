c2fd6c0bea069bb293bfbf8ffb106cef2c5062e8
ucoksayuti <ucok.sayuti567@gmail.com>
2024-06-02 12:56:31 +0700

    Menambah component card item

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,9 +4,13 @@ import '../styles/responsive.css'
 
 import { footerElemen } from './components/footer';
 import { navbarElement } from './components/navbar';
+import { cardElement } from './components/card';
 
 const footerContainer = document.querySelector('#footer');
 footerContainer.innerHTML = footerElemen;
 
 const navbarContainer = document.querySelector('#header');
 navbarContainer.innerHTML = navbarElement;
+
+const cardContainer = document.querySelector('#main-content')
+cardContainer.innerHTML = cardElement;
