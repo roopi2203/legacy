bc432132184e73e8908dbd5eea0b2d34c84d4da6
roopi2203/legacy <roofiraditya07@gmail.com>
2024-06-02 19:06:36 +0800

    Membuat history-page

---
+++ src/scripts/components/navbar.js
@@ -0,0 +1,26 @@
+const navbarElement = `
+    <div class="logo-header-con">
+    <a href="#"><img class="logo-header" src="./images/legacy-logo.png" alt=""></a>
+    </div>
+
+    <div class="navbar">
+    <ul class="nav-list">
+        <li class="nav-item"><a href="">HOME</a></li>
+        <li class="nav-item"><a href="">HISTORY</a></li>
+        <li class="nav-item"><a href="">FAQ</a></li>
+        <li class="nav-item"><a href="">ABOUT US</a></li>
+    </ul>
+    </div>
+
+    <div class="profile-header">
+    <div class="profile-images">
+        <img src="./images/profile-picture-1.jpg" alt="">
+    </div>
+    </div>
+
+
+`
+
+export {
+    navbarElement
+};
