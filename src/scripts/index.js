4feaaad8847d309cb8045e6d6c5755366e68a2c4
roopi2203/legacy <roofiraditya07@gmail.com>
2024-06-17 21:33:38 +0800

    Membuat mainpage-admin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import 'regenerator-runtime'
+import 'regenerator-runtime';
 import '../styles/main.css';
 
 import { footerElemen } from './components/footer';
@@ -9,20 +9,3 @@ footerContainer.innerHTML = footerElemen;
 
 const navbarContainer = document.querySelector('#header');
 navbarContainer.innerHTML = navbarElement;
-
-document.querySelectorAll('.accordion-header').forEach(item => {
-    item.addEventListener('click', () => {
-        const content = item.nextElementSibling;
-        const icon = item.querySelector('.plus-icon');
-        
-        if (content.classList.contains('open')) {
-            content.classList.remove('open');
-            icon.textContent = '+';
-        } else {
-            document.querySelectorAll('.accordion-content').forEach(content => content.classList.remove('open'));
-            document.querySelectorAll('.plus-icon').forEach(icon => icon.textContent = '+');
-            content.classList.add('open');
-            icon.textContent = '−';
-        }
-    });
-});
