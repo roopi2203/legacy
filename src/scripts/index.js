4c846ba9450773f57b13bf9021bf60fd62a1956c
roopi2203/legacy <roofiraditya07@gmail.com>
2024-06-17 22:14:17 +0800

    Membuat historypage_admin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,14 @@ footerContainer.innerHTML = footerElemen;
 
 const navbarContainer = document.querySelector('#header');
 navbarContainer.innerHTML = navbarElement;
+
+// document.getElementById('upload-button').addEventListener('click', function() {
+//     document.getElementById('file-upload').click();
+// });
+
+// document.getElementById('file-upload').addEventListener('change', function() {
+//     const file = this.files[0];
+//     if (file) {
+//         alert(`Selected file: ${file.name}`);
+//     }
+// });
